fix weighting of SIS; set up weighted INCRIS and remove A check

IM_domain.py
@@ -67,10 +67,10 @@ def variance_test(store_results,methods,tag,epsilon_c,epsilon_q):
                 MSE = np.mean([(score - eval_score)**2 for score in scores[method]])
                 MSEs[method].append(MSE)
         if store_results:
-            markers={"IS": "x","PDIS":"o","SIS (Lift states)":"s","SIS (Covariance testing)":"D","SIS (Q-based)": "v","INCRIS":"^",
-                     "DR": "x", "DRSIS (Lift states)": "s", "DRSIS (Covariance testing)": "D", "DRSIS (Q-based)": "v"}
-            colors={"IS": "tab:blue","PDIS":"tab:orange","SIS (Lift states)":"tab:green","SIS (Covariance testing)":"tab:red","SIS (Q-based)": "tab:purple","INCRIS":"tab:brown",
-                     "DR": "tab:blue", "DRSIS (Lift states)": "tab:green", "DRSIS (Covariance testing)": "tab:red", "DRSIS (Q-based)": "tab:purple"}
+            markers={"WIS": "x","WPDIS":"o","SIS (Lift states)":"s","WSIS (Covariance testing)":"D","WSIS (Q-based)": "v","WINCRIS":"^",
+                     "WDR": "x", "WDRSIS (Lift states)": "s", "WDRSIS (Covariance testing)": "D", "WDRSIS (Q-based)": "v"}
+            colors={"WIS": "tab:blue","WPDIS":"tab:orange","WSIS (Lift states)":"tab:green","WSIS (Covariance testing)":"tab:red","WSIS (Q-based)": "tab:purple","WINCRIS":"tab:brown",
+                     "WDR": "tab:blue", "WDRSIS (Lift states)": "tab:green", "WDRSIS (Covariance testing)": "tab:red", "WDRSIS (Q-based)": "tab:purple"}
 
             # table
             writefile=open("variance_test_"+str(MC_iterations)+tag+".txt","w")
@@ -91,6 +91,6 @@ if __name__ == '__main__':
     #convergence()
     MC_methods=["WIS","WPDIS","WSIS (Covariance testing)","WSIS (Q-based)","WINCRIS"]
     DR_methods = ["WDR", "WDRSIS (Covariance testing)", "WDRSIS (Q-based)"]
-    variance_test(methods=MC_methods, store_results=True,tag="MC_methods", epsilon_c=0.01,epsilon_q=1.0)
-    variance_test(methods=DR_methods, store_results=True, tag="DR_methods", epsilon_c=0.01,epsilon_q=1.0)
+    variance_test(methods=MC_methods, store_results=True,tag="MC_methods", epsilon_c=20.0,epsilon_q=25.0)
+    #variance_test(methods=DR_methods, store_results=True, tag="DR_methods", epsilon_c=20.0,epsilon_q=25.0)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

importance_sampling/INCRIS.py
@@ -29,7 +29,6 @@ def get_MSE(k,t,trajectories,p_e,p_b):
     Bs = []
     rs = []
     # k < t
-
     for i, trajectory in enumerate(trajectories):
         A, B, r = get_A_B_r(k, t, trajectory, p_e, p_b)
         As.append(A)
@@ -41,11 +40,15 @@ def get_MSE(k,t,trajectories,p_e,p_b):
     # print("Br",np.mean(Br))
     # print("avg A",np.mean(A))
     # print("C", np.mean(C))
-    if np.abs(np.mean(A) - 1.0) > eps:
-        return float("inf"), Br  # never choose these when mean is not close to 1.0
+    # if np.abs(np.mean(A) - 1.0) > eps:
+    #     return float("inf"), Br  # never choose these when mean is not close to 1.0
     MSE = V + C[0, 1] ** 2
-    return MSE, Br
-def INCRIS(trajectories,p_e,p_b,H,weighted=False):
+    SW=sum(Bs)
+    # print(MSE)
+    # print(Br)
+    # print(SW)
+    return MSE, Br, SW
+def INCRIS(trajectories,p_e,p_b,H,max_t=20,weighted=False):
     """
     exhaustively search for the best drop across sets of SA-pairs
     :param trajectories:
@@ -61,25 +64,28 @@ def INCRIS(trajectories,p_e,p_b,H,weighted=False):
     for t in range(0,H+1):
         best_MSE = float("inf")
         best_k = None
-        for k in range(0,t+1): # loop over possible k
-            MSE, Br = get_MSE(k, t, trajectories,p_e,p_b)
+        for k in range(0,min(max_t,t+1)): # loop over possible k
+            MSE, Br, SW = get_MSE(k, t, trajectories,p_e,p_b)
             if MSE < best_MSE:
                 best_k = k
                 best_MSE = MSE
-                r_t = np.mean(Br)
+                if weighted:
+                    r_t = np.sum(Br) / SW
+                else:
+                    r_t = np.mean(Br)
         G+=r_t
         best_ks.append(best_k)
         #print("best k", best_k)
     return G, best_ks
 
-def INCRIS_Gs(trajectories,p_e,p_b,H,best_ks,weighted=False,period=float("inf")):
-    G_scores= []
-    for num_traj in range(period,len(trajectories)+period,period):
-        G = 0
-        for t in range(H):
-            MSE, Br = get_MSE(best_ks[t], t, trajectories[0:num_traj], p_e, p_b)
-            G_t = np.mean(Br)
-            G+=G_t
-        G_scores.append(G)
-    print("INCRIS ",G_scores[-1])
-    return G_scores, best_ks
+# def INCRIS_Gs(trajectories,p_e,p_b,H,best_ks,weighted=False,period=float("inf")):
+#     G_scores= []
+#     for num_traj in range(period,len(trajectories)+period,period):
+#         G = 0
+#         for t in range(H):
+#             MSE, Br = get_MSE(best_ks[t], t, trajectories[0:num_traj], p_e, p_b)
+#             G_t = np.mean(Br)
+#             G+=G_t
+#         G_scores.append(G)
+#     print("INCRIS ",G_scores[-1])
+#     return G_scores, best_ks

importance_sampling/SIS_utils.py
@@ -44,12 +44,12 @@ def get_SIS_estimate(trajectories,p_e,p_b,Ss,weighted,period=float("inf")):
 
     return As,Bs,rs,Br,SW,scores
 
-def get_MSE(As,Br,weighted):
+def get_MSE(As,Br,weighted,SW):
     C = np.cov(As, Br)
     V = np.var(Br)
     MSE = V + C[0, 1] ** 2  # MSE is variance + bias^2
     if weighted:
-        G = np.sum(Br)
+        G = np.sum(Br)/SW
     else:
         G = np.mean(Br)
     hatA = np.mean(As)